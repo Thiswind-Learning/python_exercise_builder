b5199a9839a8c683eef34c8e2e116285830e6656
liudonghua123 <liudonghua123@gmail.com>
2020-06-17 14:56:56 +0800

    create via flutter create --org com.liudonghua python_exercise_builder

---
+++ android/app/src/main/kotlin/com/thiscloud/learning/python_exercise_builder/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.thiscloud.learning.python_exercise_builder
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ linux/window_configuration.cc
@@ -0,0 +1,5 @@
+#include "window_configuration.h"
+
+const char *kFlutterWindowTitle = "python_exercise_builder";
+const unsigned int kFlutterWindowWidth = 1280;
+const unsigned int kFlutterWindowHeight = 720;
